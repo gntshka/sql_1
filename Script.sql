809be45233cd3d3f317d8a55076fea42eeec2436
gntshka <gntshka@gmail.com>
2025-05-28 13:52:01 +0500

    Version 2

--- Script.sql
+++ Script.sql
@@ -11,6 +11,7 @@ CREATE TABLE IF NOT EXISTS artist (
 
 
 CREATE TABLE IF NOT EXISTS artist_genre (
+	PRIMARY KEY (artist_id, genre_id),
 	artist_id INTEGER NOT NULL REFERENCES artist(artist_id),
 	genre_id INTEGER NOT NULL REFERENCES genre(genre_id)
 );
@@ -19,11 +20,12 @@ CREATE TABLE IF NOT EXISTS artist_genre (
 CREATE TABLE IF NOT EXISTS albums (
 	album_id SERIAL PRIMARY KEY,
 	album VARCHAR(100) NOT NULL,
-	album_year DATE NOT NULL
+	album_year DATE CHECK(album_year >= '01.01.1900')
 );
 
 
 CREATE TABLE IF NOT EXISTS album_artist (
+	PRIMARY KEY (artist_id, album_id),
 	artist_id INTEGER NOT NULL REFERENCES artist(artist_id),
 	album_id INTEGER NOT NULL REFERENCES albums(album_id)
 );
@@ -32,7 +34,7 @@ CREATE TABLE IF NOT EXISTS album_artist (
 CREATE TABLE IF NOT EXISTS tracks (
 	track_id SERIAL PRIMARY KEY,
 	track VARCHAR(100) NOT NULL,
-	duration TIME NOT NULL,
+	duration INTEGER CHECK(duration > 30),
 	album_id INTEGER NOT NULL REFERENCES albums(album_id)
 );
 
@@ -40,6 +42,11 @@ CREATE TABLE IF NOT EXISTS tracks (
 CREATE TABLE IF NOT EXISTS collections (
 	collection_id SERIAL PRIMARY KEY,
 	collection VARCHAR(100) NOT NULL,
-	collection_year DATE NOT NULL,
-	track_id INTEGER NOT NULL REFERENCES tracks(track_id)
+	collection_year DATE CHECK(collection_year >= '01.01.1900')
+);
+
+CREATE TABLE IF NOT EXISTS tracks_collections(
+	PRIMARY KEY (track_id, collection_id),
+	track_id INTEGER NOT NULL REFERENCES tracks(track_id),
+	collection_id INTEGER NOT NULL REFERENCES collections(collection_id)
 );
